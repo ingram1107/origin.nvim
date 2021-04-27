8f04a424b08b43dd7987b4adf3aec0e1f9f1c1c3
Little Clover <littleclover909@gmail.com>
2021-04-27 18:58:04 +0800

    feature: allow customisation on project root

--- lua/origin.lua
+++ lua/origin.lua
@@ -2,10 +2,17 @@
 -- TODO:
 --    neovim should have an option to call similar api func to getcwd()
 --      - littleclover  Fri 12 Mar 2021 08:22:16 PM +08
+local ft_table = {}
 local root = vim.fn.getcwd()
 
 local function origin()
   vim.api.nvim_echo({{root, "Normal"}}, false, {})
+end
+
+local function default_source(table)
+  for key, value in pairs(table) do
+    ft_table[key] = value
+  end
 end
 
 local function set_root(dir)
@@ -13,12 +20,20 @@ local function set_root(dir)
     dir = vim.fn.expand("%:p:h")
   end
 
-  vim.api.nvim_set_current_dir(dir)
-  root = vim.fn.expand(dir)
+  local ds = ft_table[vim.bo.filetype]
+  if ds ~= nil and ds == vim.fn.substitute(vim.fn.expand(dir), "\\zs.*\\ze"..ds.."$", "", "") then
+    root = vim.fn.substitute(vim.fn.expand(dir), ".*\\zs\\/"..ds.."\\ze$", "", "")
+    vim.api.nvim_set_current_dir(root)
+  else
+    root = vim.fn.expand(dir)
+    vim.api.nvim_set_current_dir(root)
+  end
+
   vim.api.nvim_echo({{"Change root directory to `"..root.."`", "Normal"}}, true, {})
 end
 
 return {
   origin = origin,
+  default_source = default_source,
   set_root = set_root,
 }
--- plugin/origin.vim
+++ plugin/origin.vim
@@ -6,5 +6,3 @@ let g:loaded_origin=1
 
 command! -nargs=0 Origin lua require('origin').origin()
 command! -nargs=? -complete=dir OriginSetRoot lua require('origin').set_root("<args>")
-
-call execute("OriginSetRoot " . "%:p:h")
