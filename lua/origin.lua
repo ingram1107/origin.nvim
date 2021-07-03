3f568d4fea35c28ac9aa2904bbfb21b6ebe7264c
Little Clover <littleclover909@gmail.com>
2021-07-03 21:02:56 +0800

    add licence

--- lua/origin.lua
+++ lua/origin.lua
@@ -1,4 +1,18 @@
--- origin.lua
+--[[
+  origin.nvim   Nvim plugin that provide functionalities to set your cwd
+  Copyright (C) 2021  Little Clover 
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  GNU General Public License for more details. 
+  You should have received a copy of the GNU General Public License 
+  along with this program.  If not, see <https://www.gnu.org/licenses/>
+--]]
+
 -- TODO:
 --    neovim should have an option to call similar api func to getcwd()
 --      - littleclover  Fri 12 Mar 2021 08:22:16 PM +08
